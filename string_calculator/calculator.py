f44cd1e2055f97e188cb0682fe1debc549e657f0
katlegoJKS <katlego.malatjie@umuzi.org>
2020-03-05 15:41:29 +0200

    test_calculator final

--- string_calculator/calculator.py
+++ string_calculator/calculator.py
@@ -1,23 +1,36 @@
-class String_cal:
-    def __init__(self):
-        pass
+import re
 
-    def calculate_sum(self,first,second):
-        try:
-            return int(first) + int(second)
-        except ValueError:
-            return 0
+regex = re.compile(r'\d+')
 
-add = String_cal()
-add.calculate_sum("","")
-first_number = ""
-second_number = ""
-sum = add.calculate_sum(first_number,second_number)
+def has_negatives(string):
+    empty_string = ''
+    
+    for x in range(len(string)):
+        if string[x] == '-' and string[x+1].isdecimal():
+            empty_string += '-' + string[x+1] + ','
+    
+    return empty_string
 
-if sum == "":
-    # print(calculate_sum(first_number,second_number))
-    print("Conversion failed")
-else:
-    print(sum)
-
-print(add.calculate_sum("",""))
+def add(string):
+    sum = 0
+    numbers = regex.findall(string)
+    negatives = has_negatives(string)
+    
+    try:
+        string[:-1]
+    except:
+        raise "This is not ok!"
+    if negatives:
+        raise Exception("Negatives are not allowed: " + negatives)    
+    
+    if string == '':
+        return 0
+    else:
+        for x in numbers:            
+            if int(x) > 20:
+                pass
+            else:
+                num = int(x)
+                sum += num        
+        return sum
+print(add("1,2,3,4"))
--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,6 +1,32 @@
-from string_calculator.calculator import String_cal
+from string_calculator.calculator import add
+import pytest
 
-add = String_cal()
 
-def test_calculate_sum():
-    assert add.calculate_sum("10","200") == 210
+def test_add_empty_str():
+    assert add("") == 0
+    
+def test_add_one_number():
+    assert add("1") == 1
+    
+def test_add_two_numbers():
+    assert add("1,2") == 3
+
+def test_add_many_numbers():
+    assert add("1,2,3,4") == 10
+
+def test_new_lines():
+    assert add("1\n2,3") == 6
+
+def test_diff_delimiters():
+    assert add("//;\n1;2") == 3
+
+def test_check_negatives():
+    with pytest.raises(Exception) as err:
+        assert add("//;\n-1;2,-3")
+        assert str(err.value) == "Negatives not allowed: -1,-3,"
+
+def test_more_than_twenty():
+    assert add("//;\n1;2,21") == 3
+
+def test_delimeter_len():
+    assert add("//[***]\n1***2***3") == 6
